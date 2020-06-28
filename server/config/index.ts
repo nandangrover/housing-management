e48b46979a16ee0d2232cea874cd0eedf4c551a2
Nandan Grover <nandang@nandangrover.local>
2020-06-28 15:06:06 +0530

    Update to config

--- server/config/index.ts
+++ server/config/index.ts
@@ -8,5 +8,5 @@ export default {
     db: process.env.DB,
     jwtSecret: process.env.JWT_SECRET,
     port: process.env.PORT,
-    allowedOrigins: ['http://localhost:3000', 'http://yourapp.com', 'http://localhost:4020']
+    allowedOrigins: ['http://localhost:3000', 'https://housingmanagement.herokuapp.com/', 'http://localhost:4020']
 };
